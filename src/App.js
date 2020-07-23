f20ae281cabb465e98994dba47d7643cc30627c9
Elin Aarenstrup <55394675+elinaarenstrup@users.noreply.github.com>
2020-07-23 08:36:32 +0200

    first commit, create react app and cleanup.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./components/header";
+import { Nav } from "./components/nav";
+import { Footer } from "./components/footer";
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Footer />
+    </div>
+  );
+};
